94998c2c185c52f671e9e0a438d80bba9b99850b
Santiago8333 <91435566+Santiago8333@users.noreply.github.com>
2026-06-11 20:40:29 -0300

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Trabajo_Final_Mobile_Api_2026.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
