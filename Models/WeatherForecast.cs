4692cd0b493f1e89a2db13b493cb2c936803b81a
Santiago8333 <91435566+Santiago8333@users.noreply.github.com>
2026-06-21 16:28:58 -0300

    init web api

---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,6 @@
+namespace Trabajo_Final_Mobile_Api_2026.Models;
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
